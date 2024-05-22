a77cd564abf109c442f513b3a5170e8e0aa5076c
prigeo <priscillapedrette@gmail.com>
2024-05-22 20:04:23 -0300

    V. 1.1.

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 while True:
     # usuário informa os números
     x = str(input('Informe o primeiro número: ')).replace(',', '.')
-    y = str(input('Informe o segundo número: '))(',', '.')
+    y = str(input('Informe o segundo número: ')).replace(',', '.')
 
     # converte para números decimais
     x = float(x)
     y = float(y)
 
-    # informar o tipo de operação
-    print('Operações disponívris:\n')
+    print('Operações disponíveis:\n\n')
     print('"+" para somar.')
     print('"-" para subtrair.')
     print('"*" para multiplicar.')
@@ -17,17 +16,29 @@ while True:
 
     op = input('Operação desejada: ')
 
-match op:
-    case '+':
-        print(f'A soma é: {x + y}.')
-    case '-':
-        print(f'A soma é: {x - y}.')
-    case '*':
-        print(f'A soma é: {x * y}.')
-    case '/':
-        print(f'A soma é: {x / y}.')
-    case '%':
-        print(f'A soma é: {x % y}.')
-    case _:
-        print('Operação inválida')
+    match op:
+        case '+':
+            print(f'A soma é: {x + y}.')
+        case '-':
+            print(f'A subtração é: {x - y}.')
+        case '*':
+            print(f'A multiplicção é: {x * y}.')
+        case '/':
+            print(f'A divisão é: {x / y}.')
+        case '%':
+            print(f'O resto da divisão é: {x % y}.')
+        case _:
+            print('Operação inválida')
+            continue
+
+    # pergunta para o usuário se deseja continuar ou encerrar
+    continuar = input('Deseja continuar (s/n)?')   
+
+    # verifica a opção do usuário
+    if continuar == 's':
+        continue
+    elif continuar == 'n':
+        break
+    else:
+        print('Opção inválida')
         continue
